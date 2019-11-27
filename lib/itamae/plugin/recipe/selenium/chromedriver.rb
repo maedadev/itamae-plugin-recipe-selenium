fa1b577cf3e0dbb2a9e1f4152d202148681e00de
atsushi.inoue <atsushi.inoue@probizmo.co.jp>
2019-11-27 17:58:31 +0900

    refs #3 create a special purpose "resource" to determine the version of chrome and chromedriver in the correct order

--- lib/itamae/plugin/recipe/selenium/chromedriver.rb
+++ lib/itamae/plugin/recipe/selenium/chromedriver.rb
@@ -1,29 +1,7 @@
-require 'net/http'
+include_recipe './tmp_directory'
 
-host = 'chromedriver.storage.googleapis.com'
-base_url = 'https://' + host
-browser_version = run_command('sudo yum list | grep google-chrome-stable').stdout.split[1]
-browser_version = Gem::Version.new(browser_version.to_s).segments[0..2].join('.')
-driver_version_url = base_url + "/LATEST_RELEASE_#{browser_version}"
-version = Net::HTTP.get_response(URI(driver_version_url)).body
-download_url = base_url + "/#{version}/chromedriver_linux64.zip"
-header = Net::HTTP.start(host) { |http| http.head("/#{version}/chromedriver_linux64.zip") }
-etag = header['etag'][1...-1]
-
-execute "download chromedriver-#{version}" do
-  cwd '/tmp/itamae-plugin-recipe-selenium'
-  command <<-EOF
-    rm -Rf chromedriver_linux64-#{version}*
-    curl -o chromedriver_linux64-#{version}.zip #{download_url}
-  EOF
-  not_if "echo '#{etag} chromedriver_linux64-#{version}.zip' | md5sum -c -"
-end
-
-execute "install chromedriver-#{version}" do
-  cwd '/tmp/itamae-plugin-recipe-selenium'
-  command <<-EOF
-    unzip chromedriver_linux64-#{version}.zip
-    sudo mv -f chromedriver /usr/local/bin/
-  EOF
-  not_if "/usr/local/bin/chromedriver -v | grep 'ChromeDriver #{version}'"
+setup_selenium_driver "setup chromedriver" do
+  type :chrome
+  tmp_dir '/tmp/itamae-plugin-recipe-selenium'
+  install_dir '/usr/local/bin'
 end
--- lib/itamae/plugin/recipe/selenium/geckodriver.rb
+++ lib/itamae/plugin/recipe/selenium/geckodriver.rb
@@ -1,31 +1,7 @@
-require 'net/http'
+include_recipe './tmp_directory'
 
-base_url = "https://github.com/mozilla/geckodriver/releases"
-url = base_url + '/latest'
-response = nil
-5.times do
-  response = Net::HTTP.get_response(URI(url))
-
-  break unless response.is_a?(Net::HTTPRedirection) 
-  url = response['location']
-end
-version = response.uri.to_s[/[^v]*$/]
-download_url = base_url + "/download/v#{version}/geckodriver-v#{version}-linux64.tar.gz"
-
-execute "download geckodriver-#{version}" do
-  cwd '/tmp/itamae-plugin-recipe-selenium'
-  command <<-EOF
-    rm -Rf geckodriver-v#{version}-linux64*
-    curl -L -o geckodriver-v#{version}-linux64.tar.gz #{download_url}
-  EOF
-  not_if "test -e geckodriver-v#{version}-linux64.tar.gz"
-end
-
-execute "install geckodriver-#{version}" do
-  cwd '/tmp/itamae-plugin-recipe-selenium'
-  command <<-EOF
-    tar zxf geckodriver-v#{version}-linux64.tar.gz
-    sudo mv -f geckodriver /usr/local/bin/
-  EOF
-  not_if "/usr/local/bin/geckodriver -V | grep 'geckodriver #{version}'"
+setup_selenium_driver "setup geckodriver" do
+  type :gecko
+  tmp_dir '/tmp/itamae-plugin-recipe-selenium'
+  install_dir '/usr/local/bin'
 end
