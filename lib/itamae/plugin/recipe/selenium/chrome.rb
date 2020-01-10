7cf825934ec3201b02322c88987fc276161d2358
ichylinux <ichylinux@gmail.com>
2020-01-10 14:32:27 +0900

    update supported chrome versions to 77, 78, 79

--- lib/itamae/plugin/recipe/selenium/chrome.rb
+++ lib/itamae/plugin/recipe/selenium/chrome.rb
@@ -11,5 +11,5 @@ end
 
 execute 'yum update -y google-chrome-stable' do
   user 'root'
-  not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome (76|77)\.'"
+  not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome (77|78|79)\.'"
 end
--- lib/itamae/plugin/recipe/selenium/version.rb
+++ lib/itamae/plugin/recipe/selenium/version.rb
@@ -2,7 +2,7 @@ module Itamae
   module Plugin
     module Recipe
       module Selenium
-        VERSION = "0.1.2"
+        VERSION = "0.1.3"
       end
     end
   end
