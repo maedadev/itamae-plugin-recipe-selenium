f09e2a2ea5092103ed21074bd035181f8d7a5a27
ichylinux <ichylinux@gmail.com>
2022-08-15 15:07:20 +0900

    add support for chrome 103, 104. drop support for chrome 100, 101.

--- lib/itamae/plugin/recipe/selenium/chrome.rb
+++ lib/itamae/plugin/recipe/selenium/chrome.rb
@@ -18,5 +18,5 @@ end
 
 execute 'yum update -y google-chrome-stable' do
   user 'root'
-  not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome (100|101|102)\.'"
+  not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome (102|103|104)\.'"
 end
--- lib/itamae/plugin/recipe/selenium/version.rb
+++ lib/itamae/plugin/recipe/selenium/version.rb
@@ -2,7 +2,7 @@ module Itamae
   module Plugin
     module Recipe
       module Selenium
-        VERSION = '0.3.5'
+        VERSION = '0.3.6'
       end
     end
   end
