3577db8e6a65c96acd60db8ed64be3afdfe87927
ichylinux <ichylinux@gmail.com>
2024-12-21 12:27:59 +0900

    now suported chrome versions are 128, 129, 130.

--- lib/itamae/plugin/recipe/selenium/chrome.rb
+++ lib/itamae/plugin/recipe/selenium/chrome.rb
@@ -10,13 +10,16 @@ if ENV['CHROME_VERSION'].to_s.empty?
   case "#{node.platform_family}-#{node.platform_version}"
   when /rhel-7\.(.*?)/
     ENV['CHROME_VERSION'] = '125.0.6422.141'
+    execute "yum install -y https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-#{ENV['CHROME_VERSION']}-1.x86_64.rpm" do
+      user 'root'
+      not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome 125\.'"
+    end
   else
-    ENV['CHROME_VERSION'] = '128.0.6613.119'
-  end
-
-  execute "yum install -y https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-#{ENV['CHROME_VERSION']}-1.x86_64.rpm" do
-    user 'root'
-    not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome (125|126|127|128)\.'"
+    ENV['CHROME_VERSION'] = '130.0.6723.116'
+    execute "yum install -y https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-#{ENV['CHROME_VERSION']}-1.x86_64.rpm" do
+      user 'root'
+      not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome (128|129|130)\.'"
+    end
   end
 else
   execute "yum install -y https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-#{ENV['CHROME_VERSION']}-1.x86_64.rpm" do
--- lib/itamae/plugin/recipe/selenium/version.rb
+++ lib/itamae/plugin/recipe/selenium/version.rb
@@ -2,7 +2,7 @@ module Itamae
   module Plugin
     module Recipe
       module Selenium
-        VERSION = '0.4.8'
+        VERSION = '0.4.9'
       end
     end
   end
