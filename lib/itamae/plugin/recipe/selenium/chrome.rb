f65f778f2da5e1f4500140a2aaf0bb78ca9fb343
ichy <ichylinux@gmail.com>
2025-05-15 02:17:07 +0900

    supported chrome versions are 134, 135, 136

--- lib/itamae/plugin/recipe/selenium/chrome.rb
+++ lib/itamae/plugin/recipe/selenium/chrome.rb
@@ -15,10 +15,10 @@ if ENV['CHROME_VERSION'].to_s.empty?
       not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome 125\.'"
     end
   else
-    ENV['CHROME_VERSION'] = '130.0.6723.116'
+    ENV['CHROME_VERSION'] = '136.0.7103.113'
     execute "yum install -y https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-#{ENV['CHROME_VERSION']}-1.x86_64.rpm" do
       user 'root'
-      not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome (128|129|130)\.'"
+      not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome (134|135|136)\.'"
     end
   end
 else
