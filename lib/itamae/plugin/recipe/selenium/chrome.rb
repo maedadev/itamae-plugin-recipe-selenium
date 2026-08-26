1193c3fd97fe448e63316fdeaa896d637ab4170a
yuichiro.asahi <yuichiro.asahi@ulsystems.co.jp>
2026-08-26 18:53:10 +0900

    add support chrom-152

--- lib/itamae/plugin/recipe/selenium/chrome.rb
+++ lib/itamae/plugin/recipe/selenium/chrome.rb
@@ -6,24 +6,13 @@ template '/etc/yum.repos.d/google-chrome.repo' do
   variables disable_gpg_check: ['1', 'yes', 'true'].include?(ENV['CHROME_DISABLE_GPG_CHECK'].to_s.downcase)
 end
 
-if ENV['CHROME_VERSION'].to_s.empty?
-  case "#{node.platform_family}-#{node.platform_version}"
-  when /rhel-7\.(.*?)/
-    ENV['CHROME_VERSION'] = '125.0.6422.141'
-    execute "yum install -y https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-#{ENV['CHROME_VERSION']}-1.x86_64.rpm" do
-      user 'root'
-      not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome 125\.'"
-    end
-  else
-    ENV['CHROME_VERSION'] = '149.0.7827.200'
-    execute "yum install -y https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-#{ENV['CHROME_VERSION']}-1.x86_64.rpm" do
-      user 'root'
-      not_if "which google-chrome-stable && google-chrome-stable --version | egrep 'Google Chrome 149\.'"
-    end
-  end
-else
-  execute "yum install -y https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-#{ENV['CHROME_VERSION']}-1.x86_64.rpm" do
-    user 'root'
-    not_if "which google-chrome-stable && google-chrome-stable --version | cut -d ' ' -f 3 | egrep \"^#{ENV['CHROME_VERSION']}$\""
-  end
+unless node.platform_family == 'rhel' && node.platform_version.to_i >= 8
+  raise 'This recipe supports only RHEL family with version >= 8.'
+end
+
+ENV['CHROME_VERSION'] = ENV['CHROME_VERSION'].to_s.empty? ? '152.0.7977.64' : ENV['CHROME_VERSION']
+
+execute "yum install -y https://dl.google.com/linux/chrome/rpm/stable/x86_64/google-chrome-stable-#{ENV['CHROME_VERSION']}-1.x86_64.rpm" do
+  user 'root'
+  not_if "which google-chrome-stable && google-chrome-stable --version | cut -d ' ' -f 3 | egrep \"^#{Regexp.escape(ENV['CHROME_VERSION'])}$\""
 end
--- lib/itamae/plugin/recipe/selenium/version.rb
+++ lib/itamae/plugin/recipe/selenium/version.rb
@@ -2,7 +2,7 @@ module Itamae
   module Plugin
     module Recipe
       module Selenium
-        VERSION = '0.5.8'
+        VERSION = '0.5.9'
       end
     end
   end
